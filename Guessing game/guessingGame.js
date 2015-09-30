b306e36d9da29c0a84be56ccee7fe6ef47304d7d
Noland Reed <ChaseReed@Nolands-MacBook-Pro.local>
2015-09-30 13:02:46 -0700

    Guessig game code

---
+++ Guessing game/guessingGame.js
@@ -0,0 +1,35 @@
+var question1 = prompt('Is the sky blue?');
+    
+	if(question1 == 'yes'){
+    	alert("Correct, the sky is blue!");
+    } else {
+    	alert("Sorry, better luck on the next question.");
+    }
+
+
+var question2 = prompt('Is Seattle awesome?');
+    
+    if(question2 == 'yes'){
+    	alert("Correct! Seatle is in fact awesome!");
+    } else {
+    	alert("Sorry, better luck on the next question.");
+    } 
+
+
+var question3 = prompt('Is the answer to this question yes, or no?');
+
+	if(question3 == 'yes'){
+		alert("Correct, the answer is yes!");
+	} else {
+		alert("Sorry, better luck next time.");
+	}
+
+var y = 'yes'
+var n = 'no'
+
+
+
+
+    console.log('question 1 responce: ' + question1);
+	console.log('question 2 responce: ' + question2);
+	console.log('question 3 responce: ' + question3);
