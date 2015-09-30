cae50e80ed1bbc25268bf1cf500dd11567b12d32
Noland Reed <Chase.reed@mac.com>
2015-09-30 14:19:59 -0700

    changed my double quotes all to single quotes

--- guessingGame.js
+++ guessingGame.js
@@ -3,20 +3,20 @@ var counter = 0
 var question1 = prompt('Is the sky blue?');
     
 	if(question1 == 'YES'||'Yes'||'yes'||'y'||'Y'){
-    	alert("Correct, the sky is blue!");
+    	alert('Correct, the sky is blue!');
     	counter++
     } else {
-    	alert("Sorry, better luck on the next question.");
+    	alert('Sorry, better luck on the next question.');
     }
 
 
 var question2 = prompt('Is Seattle awesome?');
     
     if(question2 == 'YES'||'Yes'||'yes'||'y'||'Y'){
-    	alert("Correct! Seatle is in fact awesome!");
+    	alert('Correct! Seatle is in fact awesome!');
     	counter++
     } else {
-    	alert("Sorry, better luck on the next question.");
+    	alert('Sorry, better luck on the next question.');
     } 
 
 
@@ -26,7 +26,7 @@ var question3 = prompt('Is the answer to this question yes, or no?');
 		alert("Correct, the answer is yes!");
 		counter++
 	} else {
-		alert("Sorry, better luck next time.");
+		alert('Sorry, better luck next time.');
 	}
 
 alert('thank you for playing the Guessing Game, you got ' + counter + ' out of 3 correct. ')
