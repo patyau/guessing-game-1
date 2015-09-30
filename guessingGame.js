cd9abff41fa8c878584f41af7725c8b5d32f6e9e
Noland Reed <Chase.reed@mac.com>
2015-09-30 14:07:32 -0700

    Added counter for corect answer & added error correction for the anser YES

--- guessingGame.js
+++ guessingGame.js
@@ -1,7 +1,10 @@
+var counter = 0
+
 var question1 = prompt('Is the sky blue?');
     
-	if(question1 == 'yes && YES'){
+	if(question1 == 'YES'||'Yes'||'yes'||'y'||'Y'){
     	alert("Correct, the sky is blue!");
+    	counter++
     } else {
     	alert("Sorry, better luck on the next question.");
     }
@@ -9,8 +12,9 @@ var question1 = prompt('Is the sky blue?');
 
 var question2 = prompt('Is Seattle awesome?');
     
-    if(question2 == 'yes'){
+    if(question2 == 'YES'||'Yes'||'yes'||'y'||'Y'){
     	alert("Correct! Seatle is in fact awesome!");
+    	counter++
     } else {
     	alert("Sorry, better luck on the next question.");
     } 
@@ -18,12 +22,14 @@ var question2 = prompt('Is Seattle awesome?');
 
 var question3 = prompt('Is the answer to this question yes, or no?');
 
-	if(question3 == 'yes'){
+	if(question3 == 'YES'||'Yes'||'yes'||'y'||'Y'){
 		alert("Correct, the answer is yes!");
+		counter++
 	} else {
 		alert("Sorry, better luck next time.");
 	}
 
+alert('thank you for playing the Guessing Game, you got ' + counter + ' out of 3 correct. ')
 
 
 
