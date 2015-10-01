aa28b08fbab115296b90b42f77c1eb23a87482e2
Noland Reed <Chase.reed@mac.com>
2015-10-01 11:16:01 -0700

    added .toUpperCase to all answers and changed == to ===

--- guessingGame.js
+++ guessingGame.js
@@ -2,7 +2,7 @@ var counter = 0
 
 var question1 = prompt('Is the sky blue?');
     
-	if(question1 == 'YES'|| question1 == 'Yes'|| question1 == 'yes'|| question1 == 'y'|| question1 == 'Y'){
+	if(question1.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y') {
     	alert('Correct, the sky is blue!');
     	counter++
     } else {
@@ -12,7 +12,7 @@ console.log('question 1 responce: ' + question1);
 
 var question2 = prompt('Is Seattle awesome?');
     
-    if(question2 == 'YES'|| question2 == 'Yes'|| question2 == 'yes'|| question2 == 'y'|| question2 == 'Y'){
+    if(question2.toUpperCase() === 'YES'||question1.toUpperCase() === 'Y'){
     	alert('Correct! Seatle is in fact awesome!');
     	counter++
     } else {
@@ -22,7 +22,7 @@ console.log('question 2 responce: ' + question2);
 
 var question3 = prompt('Is the answer to this question yes, or no?');
 
-	if(question3 == 'YES'|| question3 == 'Yes'|| question3 == 'yes'|| question3 == 'y'|| question3 == 'Y'){
+	if(question3.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y'){
 		alert("Correct, the answer is yes!");
 		counter++
 	} else {
@@ -33,4 +33,3 @@ console.log('question 3 responce: ' + question3);
 
 
 alert('thank you for playing the Guessing Game, you got ' + counter + ' out of 3 correct. ')
-
