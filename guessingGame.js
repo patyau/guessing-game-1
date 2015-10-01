6ae65835f611d15ab9c5cb3e750137db098ec50b
Noland Reed <Chase.reed@mac.com>
2015-10-01 09:46:16 -0700

    changed all the answers to work correctly with truthy & falsey

--- guessingGame.js
+++ guessingGame.js
@@ -2,37 +2,35 @@ var counter = 0
 
 var question1 = prompt('Is the sky blue?');
     
-	if(question1 == 'YES'||'Yes'||'yes'||'y'||'Y'){
+	if(question1 == 'YES'|| question1 == 'Yes'|| question1 == 'yes'|| question1 == 'y'|| question1 == 'Y'){
     	alert('Correct, the sky is blue!');
     	counter++
     } else {
     	alert('Sorry, better luck on the next question.');
     }
-
+console.log('question 1 responce: ' + question1);
 
 var question2 = prompt('Is Seattle awesome?');
     
-    if(question2 == 'YES'||'Yes'||'yes'||'y'||'Y'){
+    if(question2 == 'YES'|| question2 == 'Yes'|| question2 == 'yes'|| question2 == 'y'|| question2 == 'Y'){
     	alert('Correct! Seatle is in fact awesome!');
     	counter++
     } else {
     	alert('Sorry, better luck on the next question.');
     } 
-
+console.log('question 2 responce: ' + question2);
 
 var question3 = prompt('Is the answer to this question yes, or no?');
 
-	if(question3 == 'YES'||'Yes'||'yes'||'y'||'Y'){
+	if(question3 == 'YES'|| question3 == 'Yes'|| question3 == 'yes'|| question3 == 'y'|| question3 == 'Y'){
 		alert("Correct, the answer is yes!");
 		counter++
 	} else {
 		alert('Sorry, better luck next time.');
 	}
 
+console.log('question 3 responce: ' + question3);
+
+
 alert('thank you for playing the Guessing Game, you got ' + counter + ' out of 3 correct. ')
 
-
-
-    console.log('question 1 responce: ' + question1);
-	console.log('question 2 responce: ' + question2);
-	console.log('question 3 responce: ' + question3);
