f643479694c919821a1785f2d54106ba7d60f60f
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-10-01 13:34:25 -0700

    added logic into functions and personalization and fixed slight errors

--- guessingGame.js
+++ guessingGame.js
@@ -1,35 +1,43 @@
-var counter = 0
+var userName = prompt('What is your name?');
+var counter = 0;
 
-var question1 = prompt('Is the sky blue?');
-    
-	if(question1.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y') {
-    	alert('Correct, the sky is blue!');
-    	counter++
+function ques1() {
+    var question1 = prompt('Is the sky blue?');
+    if(question1.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y') {
+        alert('Correct ' + userName + ', the sky is blue!');
+        counter++
     } else {
-    	alert('Sorry, better luck on the next question.');
+        alert('Sorry ' + userName + ', better luck on the next question.');
     }
-console.log('question 1 responce: ' + question1);
+    console.log('question 1 response: ' + question1);
+}
 
-var question2 = prompt('Is Seattle awesome?');
-    
-    if(question2.toUpperCase() === 'YES'||question1.toUpperCase() === 'Y'){
-    	alert('Correct! Seatle is in fact awesome!');
-    	counter++
+function ques2() {
+    var question2 = prompt('Is Seattle awesome?');
+    if(question2.toUpperCase() === 'YES'|| question2.toUpperCase() === 'Y'){
+        alert('Correct ' + userName + '! Seatle is in fact awesome!');
+        counter++
     } else {
-    	alert('Sorry, better luck on the next question.');
+        alert('Sorry ' + userName + ', better luck on the next question.');
     } 
-console.log('question 2 responce: ' + question2);
+    console.log('question 2 response: ' + question2);
+}
 
-var question3 = prompt('Is the answer to this question yes, or no?');
+function ques3() {
+    var question3 = prompt('Is the answer to this question yes, or no?');
+    if(question3.toUpperCase() === 'YES'|| question3.toUpperCase() === 'Y'){
+        alert('Correct ' + userName + ', the answer is yes!');
+        counter++
+    } else {
+        alert('Sorry ' + userName + ', better luck next time.');
+    }
+    console.log('question 3 response: ' + question3);
+}
 
-	if(question3.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y'){
-		alert("Correct, the answer is yes!");
-		counter++
-	} else {
-		alert('Sorry, better luck next time.');
-	}
+ques1();
+ques2();
+ques3();
 
-console.log('question 3 responce: ' + question3);
+alert('thank you for playing the Guessing Game ' + userName + ', you got ' + counter + ' out of 3 correct.')
 
 
-alert('thank you for playing the Guessing Game, you got ' + counter + ' out of 3 correct. ')
