98d49a8b450013d42abcf8520c060ad9e49a5f83
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-10-01 13:42:56 -0700

    moved alerts into dom

--- guessingGame.js
+++ guessingGame.js
@@ -1,13 +1,18 @@
 var userName = prompt('What is your name?');
 var counter = 0;
 
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var correct = document.getElementById('correct');
+
 function ques1() {
     var question1 = prompt('Is the sky blue?');
     if(question1.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y') {
-        alert('Correct ' + userName + ', the sky is blue!');
+        one.innerHTML = 'Correct ' + userName + '! the sky is blue!';
         counter++
     } else {
-        alert('Sorry ' + userName + ', better luck on the next question.');
+        one.innerHTML = 'Sorry ' + userName + ', better luck on the next question.';
     }
     console.log('question 1 response: ' + question1);
 }
@@ -15,10 +20,10 @@ function ques1() {
 function ques2() {
     var question2 = prompt('Is Seattle awesome?');
     if(question2.toUpperCase() === 'YES'|| question2.toUpperCase() === 'Y'){
-        alert('Correct ' + userName + '! Seatle is in fact awesome!');
+        two.innerHTML = 'Correct ' + userName + '! Seatle is in fact awesome!';
         counter++
     } else {
-        alert('Sorry ' + userName + ', better luck on the next question.');
+        two.innerHTML = 'Sorry ' + userName + ', better luck on the next question.';
     } 
     console.log('question 2 response: ' + question2);
 }
@@ -26,10 +31,10 @@ function ques2() {
 function ques3() {
     var question3 = prompt('Is the answer to this question yes, or no?');
     if(question3.toUpperCase() === 'YES'|| question3.toUpperCase() === 'Y'){
-        alert('Correct ' + userName + ', the answer is yes!');
+        three.innerHTML = 'Correct ' + userName + '! the answer is yes!';
         counter++
     } else {
-        alert('Sorry ' + userName + ', better luck next time.');
+        three.innerHTML = 'Sorry ' + userName + ', better luck next time.';
     }
     console.log('question 3 response: ' + question3);
 }
@@ -38,6 +43,6 @@ ques1();
 ques2();
 ques3();
 
-alert('thank you for playing the Guessing Game ' + userName + ', you got ' + counter + ' out of 3 correct.')
+correct.innerHTML = 'Thank you for playing the Guessing Game ' + userName + '! You got ' + counter + ' out of 3 correct.';
 
 
