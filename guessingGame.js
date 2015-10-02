c19ccea1f800caca103aaa576c92e0f9be907658
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-10-02 09:55:31 -0700

    terminated some lines

--- guessingGame.js
+++ guessingGame.js
@@ -9,7 +9,7 @@ function ques1() {
     var question1 = prompt('Is the sky blue?');
     if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
         one.innerHTML = 'Correct ' + userName + '! the sky is blue!';
-        counter ++
+        counter ++;
     } else {
         one.innerHTML = 'Sorry ' + userName + ', better luck on the next question.';
     }
@@ -20,7 +20,7 @@ function ques2() {
     var question2 = prompt('Is Seattle awesome?');
     if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
         two.innerHTML = 'Correct ' + userName + '! Seatle is in fact awesome!';
-        counter ++
+        counter ++;
     } else {
         two.innerHTML = 'Sorry ' + userName + ', better luck on the next question.';
     } 
@@ -31,7 +31,7 @@ function ques3() {
     var question3 = prompt('Is the answer to this question yes, or no?');
     if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
         three.innerHTML = 'Correct ' + userName + '! the answer is yes!';
-        counter ++
+        counter ++;
     } else {
         three.innerHTML = 'Sorry ' + userName + ', better luck next time.';
     }
